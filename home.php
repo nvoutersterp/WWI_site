84042c5e00f1092381b7c5dac426c9a334729138
Nick <nick@users.noreply.github.com>
2019-11-12 12:55:10 +0100

    nvt

--- home.php
+++ home.php
@@ -9,7 +9,6 @@
     <!-- CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/custom.css">
-    <
 </head>
 <body>
 <header id="header02" class="flex-header">
