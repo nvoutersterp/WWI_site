cf78d922f2e57c7db09924d341d19c2fbda34784
Hiddespenkelink <s1147038@student.windesheim.nl>
2019-11-14 18:39:14 +0100

    ..

--- plaatjestest.php
+++ plaatjestest.php
@@ -1,45 +1,44 @@
-
-<head>('content-type: image/jpeg');</head>
+<html>
+<br><br><br>
 <form action="" method="POST" enctype="multipart/form-data">
     <table>
 
     </table>
 </form>
-<input type="submit" name="verzenden">
-
+</html>
 
 
 <?php
-include "productpagina.php";
+include "function.php";
+$dbname = "wideworldimporters";
+$conn = dbconect();
 
 mysqli_select_db($conn, $dbname) or die ("could not connect");
 
 $query = mysqli_query($conn,"SELECT * FROM stockitems" );
 
-while ($row = mysqli_fetch_array($query1)) {
-    ?>
-    <tr>
-        <td> <?php echo '<img src"data:image;base64, '.base64_encode($row['image']).'"alt="Image" style="width: 100px; height: 100px; ">'</td>
-    </tr>
+while ($row = mysqli_fetch_array($query)) {
+    $result = $row;
+
+    print"<td>";?> <img src="<?php echo $row["Photo"]; ?> " height="100" width="100"> <?php echo "</td>";
 
 
-    <?php
     }
+?>
+<!--//include "function.php";-->
+<!--//$dbname = "wideworldimporters";-->
+<!--//$output = '';-->
+<!--//$conn = dbconect();-->
+<!--//$output = "";-->
+<!--//if (isset($_POST['verzenden'])){-->
+<!--//      <img height="300" width="300" src="data:image;base64,' . $row[1]. ' ">
+<!--//    mysqli_select_db($conn, $dbname) or die ("could not connect");-->
+<!--//-->
+<!--//    $query = mysqli_query($conn,"select photo from stockitems");-->
+<!--//    $rec = $query->fetch_row();-->
+<!--//    print $rec[0];-->
+<!--//-->
+<!-- echo '<img height = "60" width = "80" src="data:image/jpeg;base64,'.base64_encode( $row['Photo'] ).'"/>'-->
 
 
-//include "function.php";
-//$dbname = "wideworldimporters";
-//$output = '';
-//$conn = dbconect();
-//$output = "";
-//if (isset($_POST['verzenden'])){
-//
-//    mysqli_select_db($conn, $dbname) or die ("could not connect");
-//
-//    $query = mysqli_query($conn,"select photo from stockitems");
-//    $rec = $query->fetch_row();
-//    print $rec[0];
-//
-//
-//}
-//
+</html>
