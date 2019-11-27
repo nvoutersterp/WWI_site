9353930ef52877d29c4ddb32059cff539f03341c
Nick <nick@users.noreply.github.com>
2019-11-27 14:33:03 +0100

    nvt

--- nieuwaccount.php
+++ nieuwaccount.php
@@ -8,73 +8,84 @@ $rij = 1;
 
 mysqli_select_db($conn, $dbname) or die ("could not connect");
 ?>
-<!DOCTYPE html>
-<html lang="nl">
+    <!DOCTYPE html>
+    <html lang="nl">
 <?php printHead(); ?>
 <body>
 <?php printcategorie($conn); ?>
 
-<!-- floading header with nav -->
-<header id="header01" class="flex-header">
-    <div>
-        <a href="home.php">
-            <img src="images/wwi%20logo%20text.png" class="logo">
-        </a>
-    </div>
-
-    <div>
-        <form action="productpagina.php" method="POST">
-            <input size="30" type="search" name="search" placeholder="    Hoi, wat wil je kopen?" autocapitalize="off"
-                   autocomplete="off" spellcheck="false">
-            <input type="submit" name="submit" value=">>">
-        </form>
-    </div>
-
-    <div class="header-right">
-        <div class="menu1">
-            <img src="images/inloggen.png" class="header-right-img" onclick="openLogin()">
-            <?php if ($_SESSION['isIngelogt']) {
-                printIsIngelogt();
-            } else { ?>
-                <div class="login-popup" id="myLogin">
-                    <form action="home.php" method="post" class="login-container">
-                        inloggen
-                        <button type="button" onclick="closeLogin()">Close</button>
-                        <br>
-                        gebruikersnaam: <input type="text" name="username" style="background: gray; color: white"
-                                               required><br>
-                        wachtwoord: <input type="password" name="password" style="background: gray ; color: white" required><br>
-                        <a href="#nieuwAccount">nog geen account? maak er nu een aan!</a><br>
-                        <button type="submit">inloggen</button>
-                    </form>
-                    <script>
-                        function openLogin() {
-                            document.getElementById("myLogin").style.display = "block";
-                        }
-
-                        function closeLogin() {
-                            document.getElementById("myLogin").style.display = "none";
-                        }
-
-                    </script>
-                </div>
-            <?php } ?>
+    <!-- floading header with nav -->
+    <header id="header01" class="flex-header">
+        <div>
+            <a href="home.php">
+                <img src="images/wwi%20logo%20text.png" class="logo">
+            </a>
         </div>
-        <a class="menu1" href="#favo">
-            <img src="images/verjanglijstje.png" class="header-right-img">
-        </a>
 
-        <a class="menu1" href="#mand">
-            <img src="images/winkelmandje.png" class="header-right-img">
-        </a>
-        <?php if (isset($_SESSION['firstName'])) {
-            print ('welkom, ' . $_SESSION['firstName']);
-        } else {
-            print ('hoi, iemand');
-        }?>
-    </div>
-</header>
-<main>
-    <br><br><br><br><br><br>
+        <div>
+            <form action="productpagina.php" method="POST">
+                <input size="30" type="search" name="search" placeholder="    Hoi, wat wil je kopen?"
+                       autocapitalize="off"
+                       autocomplete="off" spellcheck="false">
+                <input type="submit" name="submit" value=">>">
+            </form>
+        </div>
 
-    <?php print (time()); ?>
+        <div class="header-right">
+            <div class="menu1">
+                <img src="images/inloggen.png" class="header-right-img" onclick="openLogin()">
+                <?php if ($_SESSION['isIngelogt']) {
+                    printIsIngelogt();
+                } else { ?>
+                    <div class="login-popup" id="myLogin">
+                        <form action="home.php" method="post" class="login-container">
+                            inloggen
+                            <button type="button" onclick="closeLogin()">Close</button>
+                            <br>
+                            gebruikersnaam: <input type="text" name="username" style="background: gray; color: white"
+                                                   required><br>
+                            wachtwoord: <input type="password" name="password" style="background: gray ; color: white"
+                                               required><br>
+                            <a href="#nieuwAccount">nog geen account? maak er nu een aan!</a><br>
+                            <button type="submit">inloggen</button>
+                        </form>
+                        <script>
+                            function openLogin() {
+                                document.getElementById("myLogin").style.display = "block";
+                            }
+
+                            function closeLogin() {
+                                document.getElementById("myLogin").style.display = "none";
+                            }
+
+                        </script>
+                    </div>
+                <?php } ?>
+            </div>
+            <a class="menu1" href="#favo">
+                <img src="images/verjanglijstje.png" class="header-right-img">
+            </a>
+
+            <a class="menu1" href="#mand">
+                <img src="images/winkelmandje.png" class="header-right-img">
+            </a>
+            <?php if (isset($_SESSION['firstName'])) {
+                print ('welkom, ' . $_SESSION['firstName']);
+            } else {
+                print ('hoi, iemand');
+            } ?>
+        </div>
+    </header>
+    <main>
+        <br><br><br><br><br><br>
+
+<?php if (time() < 12) {
+    print ("goedemorgen");
+} elseif (time() > 18) {
+    print ("goedeavond");
+} else {
+    print ("goedemiddag");
+}
+
+print (date('d-m-y h:i:s'));
+?>
